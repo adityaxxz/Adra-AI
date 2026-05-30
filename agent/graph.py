4c98d4ca7f023f5a3fd523e6170770c2f2c1724a
adityaxxz <adityaranjan5995@gmail.com>
2026-05-30 16:54:21 +0530

    added architect agent

--- agent/graph.py
+++ agent/graph.py
@@ -1,34 +1,55 @@
 from langchain_groq import ChatGroq
-from prompts import planner_prompt
-from state import Plan
+from langchain_google_genai import ChatGoogleGenerativeAI
+from prompts import planner_prompt, architect_prompt
+from state import Plan, Architect, TaskPlan
 from langgraph.constants import START, END
 from langgraph.graph import StateGraph
 
 from dotenv import load_dotenv
 load_dotenv()
 
-llm = ChatGroq(model="openai/gpt-oss-120b")
+# llm = ChatGroq(model="openai/gpt-oss-120b")
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
 user_prompt = "create a simple calculator web application"
 
 def planner_agent(state: dict) -> dict:
     user_prompt = state["user_prompt"]
-    structed_llm = llm.with_structured_output(Plan)
-    res = structed_llm.invoke(planner_prompt(user_prompt)) 
+    structured_llm = llm.with_structured_output(Plan)
+    res = structured_llm.invoke(planner_prompt(user_prompt)) 
+
+    if res is None:
+        raise ValueError("Planner didn't return a valid response")
     return {"plan": res}
+
+def architect_agent(state:dict) -> dict:
+    plan: Plan = state["plan"]
+    structured_llm = llm.with_structured_output(TaskPlan)
+    res = structured_llm.invoke(architect_prompt(plan))
+
+    if res is None:
+        raise ValueError("Architect didn't return a valid response")
+    
+    res.plan = plan     #adding the previous `plan` context
+
+    return {"task_plan": res}
+
 
 
 graph = StateGraph(dict)
 
 graph.add_node("planner", planner_agent)
-# graph.set_entry_point("planner")
+graph.add_node("architect", architect_agent)
+graph.add_node("coder", coder_agent)
 
+# graph.set_entry_point("planner")
 graph.add_edge(START, "planner")
-graph.add_edge("planner", END)
+graph.add_edge("planner", "architect")
+graph.add_edge("architect", "coder")
+graph.add_edge("coder", END)
 
 
 agent = graph.compile()
 result = agent.invoke({"user_prompt": user_prompt})
 
 print(result)
-
--- agent/state.py
+++ agent/state.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 
 class File(BaseModel):
@@ -12,3 +12,13 @@ class Plan(BaseModel):
     techstack: str = Field(description="The tech stack to be used for the app, e.g. 'python', 'javascript', 'react', 'flask', etc.")
     features: list[str] = Field(description="A list of features that the app should have, e.g. 'user authentication', 'data visualization', etc.")
     files: list[File] = Field(description="A list of files to be created, each with a 'path' and 'purpose'")
+
+class Architect(BaseModel):
+    filepath: str=Field(description="The path to the file to be modified")
+    task_description: str = Field(description="A detailed description of the task to be performed on the file, e.g. 'add user authentication', 'implement data processing logic', etc.")
+
+
+class TaskPlan(BaseModel):
+    steps: list[Architect] = Field(description="A list of steps to be taken to implement the task")
+    model_config = ConfigDict(extra="allow")   #allows extra additional fields passed during model instantiation, store within the model instance 
+
