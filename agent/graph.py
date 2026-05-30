2d301666c5b4d4b83c67ef5df6b837f614785f65
adityaxxz <adityaranjan5995@gmail.com>
2026-05-30 15:53:36 +0530

    planner agent

--- agent/graph.py
+++ agent/graph.py
@@ -1,6 +1,8 @@
 from langchain_groq import ChatGroq
 from prompts import planner_prompt
 from state import Plan
+from langgraph.constants import START, END
+from langgraph.graph import StateGraph
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,11 +11,24 @@ llm = ChatGroq(model="openai/gpt-oss-120b")
 
 user_prompt = "create a simple calculator web application"
 
-prompt = planner_prompt(user_prompt)
+def planner_agent(state: dict) -> dict:
+    user_prompt = state["user_prompt"]
+    structed_llm = llm.with_structured_output(Plan)
+    res = structed_llm.invoke(planner_prompt(user_prompt)) 
+    return {"plan": res}
 
-structed_llm = llm.with_structured_output(Plan)
 
-chat = structed_llm.invoke(planner_prompt)
+graph = StateGraph(dict)
 
-print(chat)
+graph.add_node("planner", planner_agent)
+# graph.set_entry_point("planner")
+
+graph.add_edge(START, "planner")
+graph.add_edge("planner", END)
+
+
+agent = graph.compile()
+result = agent.invoke({"user_prompt": user_prompt})
+
+print(result)
 
